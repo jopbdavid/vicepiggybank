fba9bbbdccb1aeddbbc61b10dd7f75e349a525b7
jopbdavid <jopbdavid@gmail.com>
2023-10-25 17:52:39 +0100

    Landing new img

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,7 +5,7 @@ import HeroSectionTwo from "../components/HeroSectionTwo";
 import HeroSectionThree from "../components/HeroSectionThree";
 import ParallaxImage from "../components/ParallaxImage";
 import redbarn from "../assets/redbarn.png";
-import slumdog from "../assets/slumdog.jpg";
+import redneck from "../assets/redneckRich.jpeg";
 import piggygif from "../assets/piggygif.gif";
 
 const Hero = () => {
@@ -30,21 +30,29 @@ const Hero = () => {
         <ParallaxLayer offset={0.5} speed={1} factor={4}></ParallaxLayer>
         {/* Second section */}
 
-        <ParallaxLayer sticky={{ start: 0.5, end: 2.3 }} style={{ zIndex: 2 }}>
+        <ParallaxLayer sticky={{ start: 0.5, end: 2.0 }} style={{ zIndex: 2 }}>
           <ParallaxImage image={piggygif} />
         </ParallaxLayer>
         {/* Third section */}
         <ParallaxLayer offset={0} speed={0.05}></ParallaxLayer>
         <ParallaxLayer
-          offset={2.5}
-          style={{
-            backgroundImage: `url(${slumdog})`,
-            backgroundSize: "40%",
-            backgroundPosition: "top",
-            backgroundColor: "rgb(167 243 208)",
-          }}
-          className="mt-16 bg-emerald-600"
-        ></ParallaxLayer>
+          offset={2.2}
+          style={
+            {
+              // backgroundImage: `url(${redneck})`,
+              // backgroundSize: "25%",
+              // backgroundPosition: "center",
+            }
+          }
+        >
+          <div className="w-full mx-auto bg-emerald-300 h-full flex flex-col justify-center items-center">
+            <img
+              src={redneck}
+              alt="richRedneck"
+              className="w-1/3 rounded-full mx-auto"
+            />
+          </div>
+        </ParallaxLayer>
         {/* Fourth section */}
         <ParallaxLayer offset={1.5} speed={1}>
           <HeroSectionTwo />
